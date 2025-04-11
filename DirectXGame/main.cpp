7f8b7946363e413c979c071d5b3f2355d42448fd
iKedaTakumi07 <k024g0003@g.neec.ac.jp>
2025-04-11 10:55:53 +0900

    テストコミット

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -17,6 +17,8 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// ゲームシーンの初期化
 	gameScene->Initialize();
 
+	/* タグ付け */
+
 	// メインループ
 	while (true) {
 		// エンジンの更新
