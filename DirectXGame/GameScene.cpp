1d9ec1a18fcd32532f10a25aa85a472f8fa2ddfe
iKedaTakumi07 <k024g0003@g.neec.ac.jp>
2025-04-25 16:16:45 +0900

    アフィン行列の計算を修正

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -3,7 +3,11 @@
 using namespace KamataEngine;
 
 // アフィン変換行列を高速に生成する
-Matrix4x4 MakeAffineMatrix(Vector3 scale, Vector3 rotate, Vector3 translate);
+Matrix4x4 MakeRotateXMatrix(float radian);
+Matrix4x4 MakeRotateYMatrix(float radian);
+Matrix4x4 MakeRotateZMatrix(float radian);
+Matrix4x4 Mulyiply(const Matrix4x4& m1, const Matrix4x4& m2);
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
 
 void GameScene::Initialize() { /*初期化を書く*/
 	// 3Dモデルを生成
@@ -114,21 +118,73 @@ void GameScene::Draw() {
 	Model::PostDraw();
 }
 
-// アフィン変換行列を高速に生成する
-Matrix4x4 MakeAffineMatrix(Vector3 scale, Vector3 rotate, Vector3 translate) {
+// 1.x軸回転行列
+Matrix4x4 MakeRotateXMatrix(float radian) {
 	Matrix4x4 num;
-	num.m[0][0] = scale.x * 1.0f * cosf(rotate.y) * cosf(rotate.x) * 1.0f;
-	num.m[0][1] = 0.0f;
-	num.m[0][2] = 0.0f;
-	num.m[0][3] = 0.0f;
-	num.m[1][0] = 0.0f;
-	num.m[1][1] = scale.y * cosf(rotate.x) * 1.0f * cosf(rotate.z) * 1.0f;
-	num.m[1][2] = 0.0f;
-	num.m[1][3] = 0.0f;
-	num.m[2][0] = 0.0f;
-	num.m[2][1] = 0.0f;
-	num.m[2][2] = scale.z * cosf(rotate.x) * cosf(rotate.y) * 1.0f * 1.0f;
-	num.m[2][3] = 0.0f;
+	num = {1, 0, 0, 0, 0, std::cos(radian), std::sin(radian), 0, 0, std::sin(-radian), std::cos(radian), 0, 0, 0, 0, 1};
+	return num;
+}
+
+// 2.y軸回転行列
+Matrix4x4 MakeRotateYMatrix(float radian) {
+	Matrix4x4 num;
+	num = {std::cos(radian), 0, std::sin(-radian), 0, 0, 1, 0, 0, std::sin(radian), 0, std::cos(radian), 0, 0, 0, 0, 1};
+	return num;
+}
+
+// 3.z軸回転行列
+Matrix4x4 MakeRotateZMatrix(float radian) {
+	Matrix4x4 num;
+	num = {std::cos(radian), std::sin(radian), 0, 0, std::sin(-radian), std::cos(radian), 0, 0, 0, 0, 1, 0, 0, 0, 0, 1};
+	return num;
+}
+
+// 行列の積
+Matrix4x4 Mulyiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 num;
+	num.m[0][0] = m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][0] + m1.m[0][2] * m2.m[2][0] + m1.m[0][3] * m2.m[3][0];
+	num.m[0][1] = m1.m[0][0] * m2.m[0][1] + m1.m[0][1] * m2.m[1][1] + m1.m[0][2] * m2.m[2][1] + m1.m[0][3] * m2.m[3][1];
+	num.m[0][2] = m1.m[0][0] * m2.m[0][2] + m1.m[0][1] * m2.m[1][2] + m1.m[0][2] * m2.m[2][2] + m1.m[0][3] * m2.m[3][2];
+	num.m[0][3] = m1.m[0][0] * m2.m[0][3] + m1.m[0][1] * m2.m[1][3] + m1.m[0][2] * m2.m[2][3] + m1.m[0][3] * m2.m[3][3];
+
+	num.m[1][0] = m1.m[1][0] * m2.m[0][0] + m1.m[1][1] * m2.m[1][0] + m1.m[1][2] * m2.m[2][0] + m1.m[1][3] * m2.m[3][0];
+	num.m[1][1] = m1.m[1][0] * m2.m[0][1] + m1.m[1][1] * m2.m[1][1] + m1.m[1][2] * m2.m[2][1] + m1.m[1][3] * m2.m[3][1];
+	num.m[1][2] = m1.m[1][0] * m2.m[0][2] + m1.m[1][1] * m2.m[1][2] + m1.m[1][2] * m2.m[2][2] + m1.m[1][3] * m2.m[3][2];
+	num.m[1][3] = m1.m[1][0] * m2.m[0][3] + m1.m[1][1] * m2.m[1][3] + m1.m[1][2] * m2.m[2][3] + m1.m[1][3] * m2.m[3][3];
+
+	num.m[2][0] = m1.m[2][0] * m2.m[0][0] + m1.m[2][1] * m2.m[1][0] + m1.m[2][2] * m2.m[2][0] + m1.m[2][3] * m2.m[3][0];
+	num.m[2][1] = m1.m[2][0] * m2.m[0][1] + m1.m[2][1] * m2.m[1][1] + m1.m[2][2] * m2.m[2][1] + m1.m[2][3] * m2.m[3][1];
+	num.m[2][2] = m1.m[2][0] * m2.m[0][2] + m1.m[2][1] * m2.m[1][2] + m1.m[2][2] * m2.m[2][2] + m1.m[2][3] * m2.m[3][2];
+	num.m[2][3] = m1.m[2][0] * m2.m[0][3] + m1.m[2][1] * m2.m[1][3] + m1.m[2][2] * m2.m[2][3] + m1.m[2][3] * m2.m[3][3];
+
+	num.m[3][0] = m1.m[3][0] * m2.m[0][0] + m1.m[3][1] * m2.m[1][0] + m1.m[3][2] * m2.m[2][0] + m1.m[3][3] * m2.m[3][0];
+	num.m[3][1] = m1.m[3][0] * m2.m[0][1] + m1.m[3][1] * m2.m[1][1] + m1.m[3][2] * m2.m[2][1] + m1.m[3][3] * m2.m[3][1];
+	num.m[3][2] = m1.m[3][0] * m2.m[0][2] + m1.m[3][1] * m2.m[1][2] + m1.m[3][2] * m2.m[2][2] + m1.m[3][3] * m2.m[3][2];
+	num.m[3][3] = m1.m[3][0] * m2.m[0][3] + m1.m[3][1] * m2.m[1][3] + m1.m[3][2] * m2.m[2][3] + m1.m[3][3] * m2.m[3][3];
+
+	return num;
+}
+
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
+
+	Matrix4x4 rotateX = MakeRotateXMatrix(rotate.x);
+	Matrix4x4 rotateY = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 rotateZ = MakeRotateZMatrix(rotate.z);
+	Matrix4x4 rotateXYZ = Mulyiply(rotateX, Mulyiply(rotateY, rotateZ));
+
+	Matrix4x4 num;
+	num.m[0][0] = scale.x * rotateXYZ.m[0][0];
+	num.m[0][1] = scale.x * rotateXYZ.m[0][1];
+	num.m[0][2] = scale.x * rotateXYZ.m[0][2];
+	num.m[0][3] = 0.0f * 0.0f * 0.0f * 0.0f;
+	num.m[1][0] = scale.y * rotateXYZ.m[1][0];
+	num.m[1][1] = scale.y * rotateXYZ.m[1][1];
+	num.m[1][2] = scale.y * rotateXYZ.m[1][2];
+	num.m[1][3] = 0.0f * 0.0f * 0.0f * 0.0f;
+	num.m[2][0] = scale.z * rotateXYZ.m[2][0];
+	num.m[2][1] = scale.z * rotateXYZ.m[2][1];
+	num.m[2][2] = scale.z * rotateXYZ.m[2][2];
+	num.m[2][3] = 0.0f * 0.0f * 0.0f * 0.0f;
 	num.m[3][0] = translate.x;
 	num.m[3][1] = translate.y;
 	num.m[3][2] = translate.z;
