cd67b1687406311a07d61e5b395bfb241bd2ca1a
iKedaTakumi07 <k024g0003@g.neec.ac.jp>
2025-05-16 11:25:58 +0900

    ジャンプ完了まで

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -1,9 +1,26 @@
+#define NOMINMAX
+
 #include "Player.h"
 #include "WorldTransForm.h"
+#include <algorithm>
 #include <cassert>
 #include <numbers>
 
 using namespace KamataEngine;
+
+static float easeInOutQuint(float s, float e, float t) {
+	float easedT;
+	float rotationY;
+
+	easedT = (t < 0.5 ? 8 * t * t * t * t : 1 - std::powf(-2 * t + 2, 4) / 2);
+	if (easedT >= 1.0f) {
+		easedT = 1.0f;
+	}
+
+	rotationY = (1.0f - easedT) * s + easedT * e;
+
+	return rotationY;
+}
 
 void Player::Initialize(KamataEngine::Model* model, KamataEngine::Camera* camera, const Vector3& position) {
 
@@ -22,20 +39,109 @@ void Player::Initialize(KamataEngine::Model* model, KamataEngine::Camera* camera
 }
 
 void Player::Update() {
+
+	// 着地フラグ
+	bool landing = false;
+
 	// 移動入力
 	// 左右移動操作
-	if (Input::GetInstance()->PushKey(DIK_RIGHT) || Input::GetInstance()->PushKey(DIK_LEFT)) {
+	if (onGround_) {
 
-		// 左右加速
-		Vector3 acceleration = {};
-		if (Input::GetInstance()->PushKey(DIK_RIGHT)) {
+		if (Input::GetInstance()->PushKey(DIK_RIGHT) || Input::GetInstance()->PushKey(DIK_LEFT)) {
 
-			acceleration.x += kAcceleration;
-		} else if (Input::GetInstance()->PushKey(DIK_LEFT)) {
-			acceleration.x -= kAcceleration;
+			// 左右加速
+			Vector3 acceleration = {};
+
+			if (Input::GetInstance()->PushKey(DIK_RIGHT)) {
+				// 左移動中の右入力
+				if (velocity_.x < 0.0f) {
+					// ブレーキ
+					velocity_.x *= (1.0f - kAcceleration);
+				}
+				acceleration.x += kAcceleration;
+				// 向き
+				if (lrDirection_ != LRDirection::kRight) {
+					lrDirection_ = LRDirection::kRight;
+
+					// 旋回時の向き/旋回タイマー
+					turnFirstRotationY_ = worldTransform_.rotation_.y;
+					turnTimer_ = kTimeTurn;
+				}
+
+			} else if (Input::GetInstance()->PushKey(DIK_LEFT)) {
+				// 右移動中の左入力
+				if (velocity_.x > 0.0f) {
+					// ブレーキ
+					velocity_.x *= (1.0f - kAcceleration);
+				}
+				acceleration.x -= kAcceleration;
+				// 向き
+				if (lrDirection_ != LRDirection::kleft) {
+					lrDirection_ = LRDirection::kleft;
+
+					// 旋回時の向き/旋回タイマー
+					turnFirstRotationY_ = worldTransform_.rotation_.y;
+					turnTimer_ = kTimeTurn;
+				}
+			}
+
+			// 加速/減速
+			velocity_.x += acceleration.x;
+
+			// 最大速度制限
+			velocity_.x = std::clamp(velocity_.x, -kLimitRunSpeed, kLimitRunSpeed);
+
+		} else {
+			velocity_.x *= (1.0f - kAtteleration);
 		}
-		// 加速/減速
-		velocity_.x += acceleration.x;
+
+		if (Input::GetInstance()->PushKey(DIK_UP)) {
+
+			// ジャンプ初速
+			velocity_.y += kJumpAcceleration;
+		}
+
+		// ジャンプ開始
+		if (velocity_.y > 0.0f) {
+			onGround_ = false;
+		}
+
+	} else {
+
+		// 地面との当たり班
+		if (velocity_.y < 0) {
+			if (worldTransform_.translation_.y <= 1.0f) {
+				landing = true;
+			}
+		}
+
+		// 落下速度
+		velocity_.y += -kGravityAcceleration;
+		// 落下速度制限
+		velocity_.y = std::max(velocity_.y, -kLimitFallSpeed);
+
+		// 着地
+		if (landing) {
+			// めり込み排除
+			worldTransform_.translation_.y = 1.0f;
+			// 摩擦で横方向速度が減衰
+			velocity_.x *= (1.0f - kAtteleration);
+			// 下方方向速度をリセット
+			velocity_.y = 0.0f;
+			onGround_ = true;
+		}
+	}
+
+	// 旋回制限
+
+	if (turnTimer_ > 0.0f) {
+		turnTimer_ -= 1.0f / 60.0f;
+
+		float destinationRotationYTable[] = {std::numbers::pi_v<float> / 2.0f, std::numbers::pi_v<float> * 3.0f / 2.0f};
+		// 状況に応じた角度を取得する
+		float destiationRotationY = destinationRotationYTable[static_cast<uint32_t>(lrDirection_)];
+		// 自キャラの角度を設定すル
+		worldTransform_.rotation_.y = easeInOutQuint(destiationRotationY, turnFirstRotationY_, turnTimer_);
 	}
 
 	// 移動
--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -1,9 +1,15 @@
 #pragma once
 #include "KamataEngine.h"
 
+// 左右
+enum class LRDirection {
+	kRight,
+	kleft,
+};
+
 class Player {
 public:
-	void Initialize(KamataEngine::Model* model, KamataEngine::Camera* camera, const Vector3& position);
+	void Initialize(KamataEngine::Model* model, KamataEngine::Camera* camera, const KamataEngine::Vector3& position);
 
 	void Update();
 
@@ -14,8 +20,26 @@ private:
 	KamataEngine::WorldTransform worldTransform_;
 
 	// 座標
-	Vector3 velocity_{};
-	static inline const float kAcceleration = 2.0f;
+	KamataEngine::Vector3 velocity_{};
+	// 加速/減速/最大速度
+	static inline const float kAcceleration = 0.05f;
+	static inline const float kAtteleration = 0.025f;
+	static inline const float kLimitRunSpeed = 0.08f;
+
+	// 左右向き/角度/タイマー/旋回時間
+	LRDirection lrDirection_ = LRDirection::kRight;
+	float turnFirstRotationY_ = 0.0f;
+	float turnTimer_ = 0.0f;
+	// 旋回時間
+	static inline const float kTimeTurn = 1.0f;
+
+	// 着地状態フラグ
+	bool onGround_ = true;
+
+	// 重力加速度/最大落下速度/ジャンプ初速
+	static inline const float kGravityAcceleration = 0.025f;
+	static inline const float kLimitFallSpeed = 0.075f;
+	static inline const float kJumpAcceleration = 0.5f;
 
 	// モデル
 	KamataEngine::Model* model_ = nullptr;
