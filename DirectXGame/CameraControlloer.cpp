1868b1062c0e07dd15058a919ec0900511f95b15
iKedaTakumi07 <k024g0003@g.neec.ac.jp>
2025-05-30 12:15:22 +0900

    11枚目

--- DirectXGame/CameraControlloer.cpp
+++ DirectXGame/CameraControlloer.cpp
@@ -1,4 +1,5 @@
 #include "CameraControlloer.h"
+#include "Player.h"
 
 using namespace KamataEngine;
 
@@ -6,4 +7,17 @@ void CameraControlloer::Initialize() {
 
 	// 初期化
 	camera_->Initialize();
+}
+
+void CameraControlloer::Update() {}
+
+void CameraControlloer::Reset() {
+	// 追跡対象の座尾を参照
+	const WorldTransform& targetWorldTransform = target_->GetWorldTransform();
+
+	// 対象のオフセットから計算
+	camera_->translation_.x = targetWorldTransform.translation_.x + targetOffset_.x;
+	camera_->translation_.y = targetWorldTransform.translation_.y + targetOffset_.y;
+	camera_->translation_.z = targetWorldTransform.translation_.z + targetOffset_.z;
+
 }
--- DirectXGame/CameraControlloer.h
+++ DirectXGame/CameraControlloer.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <KamataEngine.h>
 
+class Player;
+
 using namespace KamataEngine;
 
 class CameraControlloer {
@@ -15,6 +17,20 @@ public:
 	/// </summary>
 	void Update();
 
+	/// <summary>
+	/// 瞬間合わせ
+	/// </summary>
+	void Reset();
+
+	void SetTarget(Player* target) { target_ = target; };
+
 private:
+	// 追跡カメラ
 	Camera* camera_ = nullptr;
+
+	// 追跡対象
+	Player* target_ = nullptr;
+
+	// オフセット
+	Vector3 targetOffset_ = {0, 0, -15.0f};
 };
