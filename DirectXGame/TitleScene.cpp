f755c2a2d2fc6f11af3f3962f5de76e22a9720fb
iKedaTakumi07 <k024g0003@g.neec.ac.jp>
2025-07-04 10:41:37 +0900

    タイトル作成

--- DirectXGame/TitleScene.cpp
+++ DirectXGame/TitleScene.cpp
@@ -1,5 +1,8 @@
 #include "TitleScene.h"
 #include "KamataEngine.h"
+#include "Math.h"
+#include <numbers>
+
 using namespace KamataEngine;
 
 void TitleScene::Initialize() {
@@ -14,18 +17,44 @@ void TitleScene::Initialize() {
 	const float kTitleScale = 2.0f;
 
 	worldTransformTitle_.scale_ = {kTitleScale, kTitleScale, kTitleScale};
+	worldTransformTitle_.translation_.y = 0.95f * std::numbers::pi_v<float>;
+
+	worldTransformplayer_.Initialize();
 
 	const float kplayerScale = 10.0f;
 
 	worldTransformplayer_.scale_ = {kplayerScale, kplayerScale, kplayerScale};
+	worldTransformplayer_.translation_.x = -2.0f;
+	worldTransformplayer_.translation_.y = -10.0f;
 }
 
-void TitleScene::Update() {}
+void TitleScene::Update() {
+
+	counter_ += 1.0f / 60.0f;
+	counter_ = std::fmod(counter_, kTimeTitleMove);
+
+	float angle = counter_ / kTimeTitleMove * 2.0f * std::numbers::pi_v<float>;
+	worldTransformTitle_.translation_.y = std::sin(angle) + 10.0f;
+
+	camera_.TransferMatrix();
+
+	WolrdtransformUpdate(worldTransformplayer_);
+
+	WolrdtransformUpdate(worldTransformTitle_);
+}
 
 void TitleScene::Draw() {
+
+	// directXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	Model::PreDraw(dxCommon->GetCommandList());
+
 	// 描画
 	modelPlayer_->Draw(worldTransformplayer_, camera_);
 	modelTitle_->Draw(worldTransformTitle_, camera_);
+
+	Model::PostDraw();
 }
 
 TitleScene::~TitleScene() {
--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -25,6 +25,8 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	TitleScene* titleScene = new TitleScene;
 	titleScene->Initialize();
 
+	// ゲームスクリーン
+
 	// メインループ
 	while (true) {
 		// エンジンの更新
